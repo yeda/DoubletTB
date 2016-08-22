29ed55df4a18c3705f849b2460c85ef7e9d8a9de
Eda Yildirim <eda.yildirim@desy.de>
2016-08-22 11:43:52 +0200

    changin plot colors etc

--- paperplots.C
+++ paperplots.C
@@ -2,6 +2,8 @@
 
 
 {
+#include <sstream>
+
 TCanvas *cc = new TCanvas("cc","",800,600);
     cc->SetRightMargin(0.125);
     cc->SetLeftMargin(0.125);
@@ -9,7 +11,7 @@ TCanvas *cc = new TCanvas("cc","",800,600);
     cc->SetTopMargin(0.07);
 
 TFile *f = TFile::Open("output/hitmaker_134.root");
-
+TString tmp_str;
 TString ss;
 TH2D *htime;
 
@@ -46,12 +48,20 @@ cc->SaveAs(ss.Data());
 
 
 // residual plots
+TLatex *latex =new TLatex();
+    latex->SetNDC();
+    latex->SetTextFont(43);
+    latex->SetTextColor(1);
+    latex->SetTextSize(26);
+    latex->SetTextColor(kOrange+7);
+    
 
 
  TFile *_file0 = TFile::Open("output/analyze_84.root");
 TH1D * h1D;
 TF1* fgaus;
 TF1* fpol;
+    ostringstream sss;
 
 for (int i=0; i<3; i++){
 if (i==0){
@@ -83,34 +93,28 @@ else{
     h1D->GetYaxis()->SetTitleSize(0.05);
     h1D->GetYaxis()->SetTitleOffset(1.2);
 
-    h1D->SetLineWidth(2);
+    h1D->SetLineWidth(1);
     h1D->SetLineColor(kBlack);
     h1D->SetStats(0);
     h1D->SetFillColor(kOrange);
     
-    fpol->SetLineWidth(2);
+    fpol->SetLineWidth(3);
     fpol->SetLineColor(kBlue);
-    fgaus->SetLineWidth(2);
+    fgaus->SetLineWidth(3);
     fgaus->SetLineColor(kRed);
 
     double res =fpol->GetParameter(2) / sqrt(2);
     double stat_err =fpol->GetParError(2) / sqrt(2);
     double sys_err = fabs(stat_err - (fgaus->GetParError(2) / sqrt(2)));
     double res_err = stat_err+sys_err;
-
-    TLatex *latex =new TLatex();
-    latex->SetNDC();
-    latex->SetTextFont(43);
-    latex->SetTextColor(1);
-    latex->SetTextSize(26);
-    latex->SetTextColor(kOrange+7);
+cout << res <<" "<< stat_err <<" "<< sys_err <<" "<< res_err<<endl;
+cout<<"here1"<<endl;
+sss.str("");
+    sss<< string("#bf{#sigma = ")<< fixed<< setprecision(2)<< res << string(" #pm ")<<fixed<<setprecision(2)<<res_err<<string("}");
+    tmp_str = TString(sss.str());
     
-    std::ostringstream ss;
-    ss<< string("#bf{#sigma = ")<< fixed<< setprecision(2)<< res << string(" #pm ")<<fixed<<setprecision(2)<<res_err<<string("}");
-    TString tmp_str = TString(ss.str());
-
-    
-TLegend *leg = new TLegend(0.55,0.65,0.87,0.93);
+cout<<"here2"<<endl;
+TLegend *leg = new TLegend(0.55,0.65,0.87,0.92);
     leg->SetFillColor(0);
     leg->SetLineColor(0);
 
@@ -118,6 +122,7 @@ TLegend *leg = new TLegend(0.55,0.65,0.87,0.93);
 leg->AddEntry(fgaus,"Gaussian","l");
 leg->AddEntry(fpol,"Gaussian + Constant","l");
 
+cout<<"here3"<<endl;
 cc->cd();
 h1D->Draw();
 leg->Draw();
@@ -129,4 +134,5 @@ cc->SaveAs(ss.Data());
     cc->SaveAs(ss.Data());
 
 }
+
 }
